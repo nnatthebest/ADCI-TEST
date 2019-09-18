a77704dbaa25eae3b38a66c1572e0ec23c5c42a4
nnatthebest <biyakov.den@gmail.com>
2019-09-18 21:20:06 +0600

    Сделал header

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.nav-link').hover(function(){
+    var child = $(this).siblings('.nav-block__child');
+    var disChild = child.css('display');
+    if(disChild == 'flex'){
+      return;
+    }else{
+      $('.nav-block__child').hide();
+      child.css('display', 'flex');
+    }
+  })
+  $('.nav-block__child').mouseleave(function(){
+    $(this).hide();
+  })
+})
