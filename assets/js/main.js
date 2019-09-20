e2ea1e9f9d72ced0c414956b0d295f1503589d24
nnatthebest <biyakov.den@gmail.com>
2019-09-20 22:37:51 +0600

    Доделал модальное окно и сделал адаптив

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,27 @@ $(document).ready(function(){
   $('.nav-block__child').mouseleave(function(){
     $(this).hide();
   })
+  $('.mobile-menu__nav .nav-link').click(function(){
+    $(this).siblings('.nav-block__child').slideToggle();
+  })
+  $('.mobile-menu__close').click(function(){
+    $('.mobile-bg').hide();
+  })
+  $('#menuToggle').click(function(){
+    $('.mobile-bg').show();
+  })
+  if($(document).width() < '400'){
+    $('.mobile-menu__close img').attr('src', '/assets/images/cansel(black).png')
+  }else{
+    $('.mobile-menu__close img').attr('src', '/assets/images/cancel.png')
+  }
+  $(window).resize(function(){
+    if($(document).width() < '400'){
+      $('.mobile-menu__close img').attr('src', '/assets/images/cansel(black).png')
+    }else{
+      $('.mobile-menu__close img').attr('src', '/assets/images/cancel.png')
+    }
+  })
   $('.slider-photos').slick({
     autoplay: true,
     arrows: false,
@@ -26,5 +47,11 @@ $(document).ready(function(){
       $('.slider-toogle__arrow img').addClass('rotate');
     }
     $('.slider-toogle__hiden').slideToggle('slow');
+  })
+  $('.popup__close').click(function(){
+    $('.popup').hide();
+  })
+  $('.header-block__menu__auth .small-link').click(function(){
+    $('.popup').css('display', 'flex');
   })
 })
